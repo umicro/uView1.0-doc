45d148f262a19874d44c62b08793924adc92436c
yiruiwen <yiruiwen@uviewui.com>
2023-08-08 11:34:32 +0800

    docs📝: update docs.

--- docs/.vuepress/theme/components/Sidebar.vue
+++ docs/.vuepress/theme/components/Sidebar.vue
@@ -1,91 +1,106 @@
 <template>
-	<div>
-		<aside class="sidebar">
-			<a target="_blank" class="jump-link" href="https://cloud.alipay.com/main/home?from=uview">
-				<img src="https://cdn.uviewui.com/uview/resources/alicloud.jpg" />
-			</a>
-			<a target="_blank" class="jump-link" href="https://v3.hhyang.cn/">
-				<img src="https://cdn.uviewui.com/uview/resources/12861689322612.jpg" />
-			</a>
-			<!-- <a target="_blank" class="jump-link" href="http://tool.zjhlone.com/sms.php">
-				<img src="https://tool.zjhlone.com/3.jpg" />
-			</a> -->
-			<NavLinks />
-			<slot name="top" />
-			<SidebarLinks :depth="0" :items="items" />
-			<slot name="bottom" />
-		</aside>
-	</div>
+    <div>
+        <aside class="sidebar">
+            <a
+                target="_blank"
+                class="jump-link"
+                href="https://www.crmeb.com/?from=uview"
+            >
+                <img src="https://cdn.uviewui.com/uview/resources/3c56cbc6216b47db61e37090fdb6619.jpg" />
+            </a>
+            <a
+                target="_blank"
+                class="jump-link"
+                href="https://cloud.alipay.com/main/home?from=uview"
+            >
+                <img src="https://cdn.uviewui.com/uview/resources/alicloud.jpg" />
+            </a>
+            <a
+                target="_blank"
+                class="jump-link"
+                href="https://www.hhyang.cn/"
+            >
+                <img src="https://cdn.uviewui.com/uview/resources/12861689322612.jpg" />
+            </a>
+            <NavLinks />
+            <slot name="top" />
+            <SidebarLinks
+                :depth="0"
+                :items="items"
+            />
+            <slot name="bottom" />
+        </aside>
+    </div>
 </template>
 
 <script>
-	import SidebarLinks from '@theme/components/SidebarLinks.vue';
-	import NavLinks from '@theme/components/NavLinks.vue';
+import SidebarLinks from '@theme/components/SidebarLinks.vue';
+import NavLinks from '@theme/components/NavLinks.vue';
 
-	export default {
-		name: 'Sidebar',
+export default {
+    name: 'Sidebar',
 
-		components: {
-			SidebarLinks,
-			NavLinks
-		},
-		data() {
-			return {
-				// 广告信息
-				ad: [
-					{
-						name: {
-							src: 'https://cdn.uviewui.com/uview/resources/7abbfb97681216fd76ed9accc68490e.png',
-							url: 'https://cool-js.com'
-						},
-						chance: 0.33
-					},
-					{
-						name: {
-							src: '/customer/contact3.png',
-							url: 'https://ext.dcloud.net.cn/plugin?id=1427'
-						},
-						chance: 0.67
-					}
-				],
-                // 顶部广告图
-                url: '/customer/contact1.png'
-			}
-		},
-		props: ['items'],
-		created() {
-			// 概率计算
-			//this.jumpInfo = this.random();
-			// 每隔一定时间，执行一次随机概率
-			// setInterval(() => {
-			// 	this.jumpInfo = this.random();
-            // }, 3000);
-            // 与广州亿速云的广告合作到期时间为2020-11-22，时间戳为：1608911994000(ms)
-            // if (+new Date() > 1606058735000) {
-            //     this.url = '/customer/contact1.png'
-            // } else {
-            //     this.url = 'https://cdn.uviewui.com/uview/friends-link/apipost_350x150px.jpeg';
-            // }
-		},
-		methods: {
-			// 根据概率，获得结果
-			random() {
-				let sum = 0,
-					factor = 0,
-					random = Math.random();
+    components: {
+        SidebarLinks,
+        NavLinks
+    },
+    data() {
+        return {
+            // 广告信息
+            ad: [
+                {
+                    name: {
+                        src: 'https://cdn.uviewui.com/uview/resources/7abbfb97681216fd76ed9accc68490e.png',
+                        url: 'https://cool-js.com'
+                    },
+                    chance: 0.33
+                },
+                {
+                    name: {
+                        src: '/customer/contact3.png',
+                        url: 'https://ext.dcloud.net.cn/plugin?id=1427'
+                    },
+                    chance: 0.67
+                }
+            ],
+            // 顶部广告图
+            url: '/customer/contact1.png'
+        }
+    },
+    props: ['items'],
+    created() {
+        // 概率计算
+        //this.jumpInfo = this.random();
+        // 每隔一定时间，执行一次随机概率
+        // setInterval(() => {
+        // 	this.jumpInfo = this.random();
+        // }, 3000);
+        // 与广州亿速云的广告合作到期时间为2020-11-22，时间戳为：1608911994000(ms)
+        // if (+new Date() > 1606058735000) {
+        //     this.url = '/customer/contact1.png'
+        // } else {
+        //     this.url = 'https://cdn.uviewui.com/uview/friends-link/apipost_350x150px.jpeg';
+        // }
+    },
+    methods: {
+        // 根据概率，获得结果
+        random() {
+            let sum = 0,
+                factor = 0,
+                random = Math.random();
 
-				for (let i = 0; i < this.ad.length; i++) {
-					sum += this.ad[i].chance; // 统计概率总和
-				};
-				random *= sum; // 生成概率随机数
-				for (let i = 0; i < this.ad.length; i++) {
-					factor += this.ad[i].chance;
-					if (factor > random) return this.ad[i].name;
-				};
-				return null;
-			}
-		}
-	};
+            for (let i = 0; i < this.ad.length; i++) {
+                sum += this.ad[i].chance; // 统计概率总和
+            };
+            random *= sum; // 生成概率随机数
+            for (let i = 0; i < this.ad.length; i++) {
+                factor += this.ad[i].chance;
+                if (factor > random) return this.ad[i].name;
+            };
+            return null;
+        }
+    }
+};
 </script>
 
 <style lang="stylus">
